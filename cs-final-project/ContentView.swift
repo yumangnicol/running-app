4b84758f47e273185ccdc15bffe8588aabf71368
nicolluisyumang <yumangnicol@gmail.com>
2023-08-09 13:05:39 +0100

    Initial Commit

---
+++ cs-final-project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  cs-final-project
+//
+//  Created by Nicol Luis Yumang on 8/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cs-final-project/cs_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs_final_projectApp.swift
+//  cs-final-project
+//
+//  Created by Nicol Luis Yumang on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct cs_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
