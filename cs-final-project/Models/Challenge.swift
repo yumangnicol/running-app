8674d90b1d720ca22ae8bdacb1ac73713b1cbedf
nicolluisyumang <yumangnicol@gmail.com>
2023-08-17 15:59:21 +0100

    feat(model): add Challenge model that represents a challenge with a goal distance in the app

---
+++ cs-final-project/Models/Challenge.swift
@@ -0,0 +1,15 @@
+//
+//  Challenge.swift
+//  cs-final-project
+//
+//  Created by Nicol Luis Yumang on 8/17/23.
+//
+
+import Foundation
+
+
+struct Challenge: Identifiable {
+    let id: UUID
+    let name: String
+    let goalDistance: Double
+}
