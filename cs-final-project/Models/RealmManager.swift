53f7b946c349249f292c076cf0efde803ae5ccc4
nicolluisyumang <yumangnicol@gmail.com>
2023-08-23 16:30:16 +0100

    feat(model): - add Default Challenges struct which identifies the three challenges for the app - add RealmManager class which handles all configuration to Realm

---
+++ cs-final-project/Models/RealmManager.swift
@@ -0,0 +1,30 @@
+//
+//  RealmManager.swift
+//  cs-final-project
+//
+//  Created by Nicol Luis Yumang on 8/23/23.
+//
+
+import Foundation
+import RealmSwift
+
+
+class RealmManager {
+    static var config: Realm.Configuration {
+        Realm.Configuration(schemaVersion: 1)
+    }
+    
+    static func setChallenges() {
+        let realm = try! Realm()
+        
+        if realm.objects(Challenge.self).isEmpty {
+            do {
+                try realm.write {
+                    realm.add(DefaultChallenges.challenges)
+                }
+            } catch {
+                print("Error adding Challenges to Realm", error)
+            }
+        }
+    }
+}
