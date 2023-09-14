e0067f0239851c223a4412005045aab10856b39c
nicolluisyumang <yumangnicol@gmail.com>
2023-09-14 21:35:07 +0100

    docs(Services): add documentation to HealthService and RealmService

--- cs-final-project/Services/HealthService.swift
+++ cs-final-project/Services/HealthService.swift
@@ -9,16 +9,22 @@ import Foundation
 import HealthKit
 import SwiftUI
 
-
+/// A service that communicates with HealthKit
 class HealthService: ObservableObject {
+    
+    /// The main data source of Workouts
     private let healthStore = HKHealthStore()
     
+    /// The singleton HealthService instance
     public static let shared = HealthService()
     
+    /// The encoded anchor value store in UserDefaults
     @AppStorage(K.Keys.anchor) private var encodedAnchor: Data?
+    
+    /// The decoded anchor value retrieved from UserDefaults
     private var serviceAnchor: HKQueryAnchor? {
         if let encodedAnchor {
-            // Decode the data to get HKQueryAnchor
+            // Decodes the data to get HKQueryAnchor
             if let decodedAnchor = try? NSKeyedUnarchiver.unarchivedObject(ofClass: HKQueryAnchor.self, from: encodedAnchor){
                 return decodedAnchor
             }
@@ -26,7 +32,10 @@ class HealthService: ObservableObject {
         return nil
     }
     
+    
+    /// Requests authorisation to use HealthKit on Device
     func requestAuthorization() {
+        // Checks if HealthKit is availabale on Device
         guard HKHealthStore.isHealthDataAvailable() else {
             print("HealthKit is not available on this device.")
             return
@@ -34,7 +43,8 @@ class HealthService: ObservableObject {
         
         // Defines the types of HKObjects to be queried
         let readTypes: Set<HKObjectType> = [.workoutType()]
-        
+                
+        // Actual authorization request
         healthStore.requestAuthorization(toShare: nil, read: readTypes) { success, error in
             guard success, error == nil else {
                 if let e = error {
@@ -45,20 +55,30 @@ class HealthService: ObservableObject {
         }
     }
     
+    
+    /// Fetches Running Workouts from HealthKit
     func fetchRunningWorkouts(completion: @escaping ([Activity]?, Error?) -> Void) {
+        
+        // A predicate to the query that determines the type of Workout to be obtained: Running
         let typePredicate = HKQuery.predicateForWorkouts(with: .running)
         
-        let dayBRO = Calendar.current.startOfDay(for: (UserDefaults.standard.object(forKey: K.Keys.firstAppLaunch)) as! Date)
-        let datePredicate = HKQuery.predicateForSamples(withStart: dayBRO, end: nil)
+        // Determines the start date of the query
+        // Uses the stored Date from UserDefaults that contains the application's first launch date
+        let firstAppLaunch = Calendar.current.startOfDay(for: (UserDefaults.standard.object(forKey: K.Keys.firstAppLaunch)) as! Date)
+        
+        // A predicate to the query that determines the date range of workouts to be obtained
+        let datePredicate = HKQuery.predicateForSamples(withStart: firstAppLaunch, end: nil)
                 
-                        
+        // Combines the typePredicate and datePredicate into a compond predicate
         let predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [typePredicate,datePredicate])
         
+        // The actual query to HealthKit to obtain running workouts
         let query = HKAnchoredObjectQuery(type: .workoutType(),
                                           predicate: predicate,
                                           anchor: serviceAnchor,
                                           limit: HKObjectQueryNoLimit) { query, samples, deletedSamples, newAnchor, error in
             
+            // Checks if there are any new workouts and if there are any errors
             guard let workouts = samples as? [HKWorkout], error == nil, let newAnchor else {
                 if let e = error {
                     print("Error fetching running workouts: \(e.localizedDescription)")
@@ -67,12 +87,12 @@ class HealthService: ObservableObject {
                 return
             }
                         
-            // Store new Anchor
+            // Encodes new Anchor and stores to User Defaults
             if let newEncodedAnchor = try? NSKeyedArchiver.archivedData(withRootObject: newAnchor, requiringSecureCoding: true) {
-                // Store the encoded anchor data in UserDefaults
                 self.encodedAnchor = newEncodedAnchor
             }
-                        
+                         
+            // Maps each workout object from HealthKit as an Activity
             let activities = workouts.map { workout in
                 let distance = workout.statistics(for: HKQuantityType(.distanceWalkingRunning))?.sumQuantity()?.doubleValue(for: .meter()) ?? 0.0
                 
--- cs-final-project/Services/RealmService.swift
+++ cs-final-project/Services/RealmService.swift
@@ -8,10 +8,13 @@
 import Foundation
 import RealmSwift
 
-
+/// A service that communicates with Realm
 class RealmService {
+    
+    /// An instance of Realm
     var realm: Realm?
     
+    /// The default Challenges to be stored on realm at first initialization
     private var defaultChallenges: [Challenge] {
         let first10K = Challenge(value: ["name": "First 10K", "goalDistance": 10000.00, "status": ChallengeStatus.incomplete] as [String : Any])
         
@@ -22,15 +25,18 @@ class RealmService {
         return [first10K, ultra100K, runToMordor]
     }
     
+    /// Initializes a RealmService
     init() {
         Realm.Configuration.defaultConfiguration = self.config
         realm = try! Realm()        
     }
     
+    /// Sets the Realm to use a default configuration
     var config: Realm.Configuration {
         Realm.Configuration(schemaVersion: 0)
     }            
     
+    /// Loads the defaultChallenges to the default Realm instance
     func loadDefaultChallenges() {
         if let realm = realm {
             if realm.objects(Challenge.self).isEmpty {
